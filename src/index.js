dcce2d9a7cfcb9399380f6c35464da4414a4ac2f
Andrecio Bezerra <andreciobezerra@gmail.com>
2020-04-15 16:00:36 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+ 
+const rootElement = document.querySelector('#root')
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, rootElement)
